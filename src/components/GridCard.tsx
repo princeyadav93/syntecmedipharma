ec270c80ef2b6ced40d92ecc2f8d31b449c45ec9
princeyadav93 <princeyaduvanshi69@gmail.com>
2025-10-28 15:51:40 +0530

    added more products category

--- src/components/GridCard.tsx
+++ src/components/GridCard.tsx
@@ -1,5 +1,7 @@
 'use client';
+import { useEffect } from 'react';
 import FeatureCard from './FeatureCard';
+import { useProductStore } from '@/store/useProductStore';
 
 const features = [
     {
@@ -45,9 +47,31 @@ const features = [
         image: '/assets/creams.jpg',
         href: '/products?category=creams',
     },
+    {
+        title: 'Medicated Soaps',
+        description: `Our range of medicated soaps is specially formulated to help combat fungal and bacterial skin infections while keeping your skin clean, healthy, and refreshed. Enriched with clinically proven active ingredients, these soaps effectively treat conditions such as dandruff, ringworm, and other skin irritations. Ideal for everyday use, they gently cleanse while providing protection, promoting smoother and clearer skin. Perfect for maintaining hygiene and preventing infection.`,
+        image: '/assets/soap.jpg',
+        href: '/products?category=soaps',
+    },
+    {
+        title: 'Advanced Probiotic Sachets',
+        description: `Our range of pre and probiotic sachets is formulated to support a healthy digestive system and restore natural gut flora. Each sachet helps improve digestion, boost nutrient absorption, and strengthen immunity. Ideal for daily use to maintain digestive comfort and overall well-being.`,
+        image: '/assets/sachets.avif',
+        href: '/products?category=sachets',
+    },
+    {
+        title: 'Refreshing Medicated Mouthwash',
+        description: `Our range of medicated mouthwashes is formulated to ensure long-lasting freshness and protection against germs. They help reduce plaque, prevent bad breath, and maintain healthy gums. Ideal for daily oral care, these mouthwashes leave your mouth clean, refreshed, and protected.`,
+        image: '/assets/mouthwashs.avif',
+        href: '/products?category=mouthwash',
+    },
 ];
 
 export default function GridCard() {
+    const { fetchProducts } = useProductStore();
+    useEffect(() => {
+        fetchProducts();
+    }, [fetchProducts]);
     return (
         <section className="w-full bg-[var(--color-bg)] mt-6 md:mt-12 text-[var(--color-text)]">
             <div className="max-w-7xl mx-auto px-6">
